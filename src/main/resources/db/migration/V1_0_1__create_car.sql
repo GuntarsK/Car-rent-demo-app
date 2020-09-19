9dcc46c75d3713e744419d06c8cd4f8249c80bfe
Guntars Kirovainis <guntars.kirovainis@gmail.com>
2020-09-19 22:45:15 +0300

    Adding couple more car models to the table

--- src/main/resources/db/migration/V1_0_1__create_car.sql
+++ src/main/resources/db/migration/V1_0_1__create_car.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS car (
     mileage INTEGER NOT NULL,
     amount DOUBLE NOT NULL,
     status VARCHAR(15) DEFAULT 'AVAILABLE',
-    status_in_db VARCHAR(10) DEFAULT 'ACTIVE'
+    status_in_db VARCHAR(15) DEFAULT 'ACTIVE'
 );
 
 
@@ -17,5 +17,9 @@ INSERT INTO car (make, model, car_body_type, year_of_production, mileage, amount
 ('Renault', 'Trafic', 'VAN', 2016, 145029, 58.00),
 ('BMW', 'X3', 'SUV', 2018, 116023, 73.00),
 ('KIA', 'Sportage', 'SUV', 2017, 128312, 47.00);
+('BMW', '320', 'SEDAN', 2019, 33054, 95.00);
+('Audi', 'A6', 'SEDAN', 2016, 58312, 88.00);
+('Volvo', 'XC90', 'SUV', 2018, 60312, 109.00);
+('Volvo', 'XC60', 'SUV', 2019, 25312, 94.00);
 
 
